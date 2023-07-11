491fc30fd15aa3c0fea57cc5ca16738993b2dbd7
Ebrahim Elsayed Elgharib <ebrahimelsayed010.com>
2023-07-11 10:03:08 +0300

    add views functions post_list & post_detail

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from .models import Post
 # Create your views here.
+
+
+def post_list(request):
+    data = Post.objects.all()
+    return render(request, 'post_list.html', context={'posts':data})
+
+
+def post_detail(request, post_id):
+    data = Post.objects.get(id=post_id)
+    return render(request, 'post_detail.html', context={'post':data})
--- project/urls.py
+++ project/urls.py
@@ -20,8 +20,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from blog.views import post_list, post_detail
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', post_list),
+    path('blog/<int:post_id>', post_detail)
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

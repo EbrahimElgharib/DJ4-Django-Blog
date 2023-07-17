5879e0a1cbd39509b8f178fac009b967c03bf9f6
Ebrahim Elsayed Elgharib <ebrahimelsayed010.com>
2023-07-17 15:33:23 +0300

    add new_post view and form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Post
+
+from .forms import PostForm
 # Create your views here.
 
 
@@ -12,3 +14,22 @@ def post_list(request):
 def post_detail(request, post_id):
     data = Post.objects.get(id=post_id)
     return render(request, 'post_detail.html', context={'post':data})
+
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/blog/')
+        
+    else:
+        form = PostForm()
+        
+    return render(request, 'new_post.html', context={'form':form})
+
+def  edit_post(request, post_id):
+    pass
+
+def delete_post(request, post_id):
+    pass
--- project/urls.py
+++ project/urls.py
@@ -20,13 +20,18 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from blog.views import post_list, post_detail
+from blog.views import post_list, post_detail, post_new, edit_post, delete_post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', post_list),
-    path('blog/<int:post_id>', post_detail)
+    path('blog/new', post_new),
+    path('blog/<int:post_id>', post_detail),
+    path('blog/<int:post_id>/edit', edit_post),
+    path('blog/<int:post_id>/delete', delete_post),
+    
 ]
+
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
